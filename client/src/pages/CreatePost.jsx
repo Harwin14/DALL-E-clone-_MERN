6b5f8a0ce56d93aa4d4c60440d604c38f060b293
Harwin14 <agungharwinn@gmail.com>
2023-06-06 18:13:25 +0700

    bug when post(cloudinary) mongose err, still after changed dns to dns google

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
                 setGeneratingImg(true);
                 const response = await fetch(
                     "http://localhost:8080/api/v1/dalle",
-                    {
+                    { 
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json",
